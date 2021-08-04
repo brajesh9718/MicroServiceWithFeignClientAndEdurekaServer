bbcf7ac82b3f96e7c66be64562131f9e096dd22e
Brajesh Pandey <pandey.brajesh9718@gmail.com>
2021-08-04 14:45:25 +0530

    employee-microservice

---
+++ employee-microservice/src/main/java/com/employee/client/Employee.java
@@ -0,0 +1,41 @@
+package com.employee.client;
+
+public class Employee{
+
+	
+	int eid;
+	String name;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int eid, String name) {
+		super();
+		this.eid = eid;
+		this.name = name;
+	}
+	
+	
+	public int getEid() {
+		return eid;
+	}
+
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", name=" + name + "]";
+	}
+
+}
